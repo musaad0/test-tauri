ee84072a88a42129c742f69987316c4492ddb881
Musaad Alqubayl <musaadalqubayl@gmail.com>
2023-06-27 13:20:57 +0300

    correct virtualized

--- src/pages/Player/VirtualImageList.tsx
+++ src/pages/Player/VirtualImageList.tsx
@@ -1,98 +1,9 @@
 import * as React from "react";
-import { createRoot } from "react-dom/client";
-
-import { useVirtualizer, useWindowVirtualizer } from "@tanstack/react-virtual";
+import { useWindowVirtualizer } from "@tanstack/react-virtual";
 import { IFile } from "@/types";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
 import { usePlayerStore } from "@/store/playerStore";
-
-// import "./index.css";
-
-// function RowVirtualizerDynamic() {
-//   const parentRef = React.useRef<HTMLDivElement>(null);
-
-//   const count = sentences.length;
-//   const virtualizer = useVirtualizer({
-//     count,
-//     getScrollElement: () => parentRef.current,
-//     estimateSize: () => 45,
-//   });
-
-//   const items = virtualizer.getVirtualItems();
-
-//   return (
-//     <div>
-//       <button
-//         onClick={() => {
-//           virtualizer.scrollToIndex(0);
-//         }}
-//       >
-//         scroll to the top
-//       </button>
-//       <span style={{ padding: "0 4px" }} />
-//       <button
-//         onClick={() => {
-//           virtualizer.scrollToIndex(count / 2);
-//         }}
-//       >
-//         scroll to the middle
-//       </button>
-//       <span style={{ padding: "0 4px" }} />
-//       <button
-//         onClick={() => {
-//           virtualizer.scrollToIndex(count - 1);
-//         }}
-//       >
-//         scroll to the end
-//       </button>
-//       <hr />
-//       <div
-//         ref={parentRef}
-//         className="List"
-//         style={{
-//           height: 400,
-//           width: 400,
-//           overflowY: "auto",
-//           contain: "strict",
-//         }}
-//       >
-//         <div
-//           style={{
-//             height: virtualizer.getTotalSize(),
-//             width: "100%",
-//             position: "relative",
-//           }}
-//         >
-//           <div
-//             style={{
-//               position: "absolute",
-//               top: 0,
-//               left: 0,
-//               width: "100%",
-//               transform: `translateY(${items[0].start}px)`,
-//             }}
-//           >
-//             {items.map((virtualRow) => (
-//               <div
-//                 key={virtualRow.key}
-//                 data-index={virtualRow.index}
-//                 ref={virtualizer.measureElement}
-//                 className={
-//                   virtualRow.index % 2 ? "ListItemOdd" : "ListItemEven"
-//                 }
-//               >
-//                 <div style={{ padding: "10px 0" }}>
-//                   <div>Row {virtualRow.index}</div>
-//                   <div>{sentences[virtualRow.index]}</div>
-//                 </div>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+import { cn } from "@/utils";
 
 export const RowVirtualizerDynamicWindow = ({ files }: { files: IFile[] }) => {
   const parentRef = React.useRef<HTMLDivElement>(null);
@@ -106,12 +17,20 @@ export const RowVirtualizerDynamicWindow = ({ files }: { files: IFile[] }) => {
 
   const virtualizer = useWindowVirtualizer({
     count: files.length,
-    overscan: 5,
+    overscan: 8,
     estimateSize: React.useCallback(() => 400, []),
     scrollMargin: parentOffsetRef.current,
   });
 
   const items = virtualizer.getVirtualItems();
+
+  // const [paddingTop, paddingBottom] =
+  //   items.length > 0
+  //     ? [
+  //         Math.max(0, items[0].start - virtualizer.options.scrollMargin),
+  //         Math.max(0, virtualizer.getTotalSize() - items[items.length - 1].end),
+  //       ]
+  //     : [0, 0];
 
   React.useEffect(() => {
     virtualizer.scrollToIndex(index);
@@ -131,6 +50,8 @@ export const RowVirtualizerDynamicWindow = ({ files }: { files: IFile[] }) => {
           style={{
             position: "absolute",
             top: 0,
+            // paddingTop,
+            // paddingBottom,
             height: "100vh",
             left: 0,
             width: "100%",
@@ -145,9 +66,9 @@ export const RowVirtualizerDynamicWindow = ({ files }: { files: IFile[] }) => {
               data-index={virtualRow.index}
               ref={virtualizer.measureElement}
             >
-              <div className="h-screen">
+              <div className={cn("h-screen")}>
                 <img
-                  className="h-full max-w-full object-contain"
+                  className="h-full max-w-full object-contain mx-auto"
                   src={convertFileSrc(files[virtualRow.index].path)}
                 />
               </div>
